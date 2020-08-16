1af5573719dbc782935d417ed2f6c0b9d2e7f0f7
lisagomez <lisagomez967@gmail.com>
2020-08-16 16:52:53 -0500

    initial

--- registros/settings.py
+++ registros/settings.py
@@ -134,6 +134,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'onlineshop/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'registros/static')]
 
 django_heroku.settings(locals())
